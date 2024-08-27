055c38ea89871520d62ab05c3ec9f5ebe84faa19
Ahmet Palavan <ahmetpalavann@gmail.com>
2024-08-27 21:58:48 +0300

    feat: Added a new QueryClientProvider component and implemented useMountedState in Modals component.

--- provider/modal-provider.tsx
+++ provider/modal-provider.tsx
@@ -1,6 +1,13 @@
+'use client';
+
 import CreateWorkspaceModal from '~/features/workspaces/components/create-workspace-modal';
+import { useMountedState } from 'react-use';
 
 export const Modals = () => {
+  const mountedState = useMountedState();
+
+  if (!mountedState) return null;
+
   return (
     <>
       <CreateWorkspaceModal />
