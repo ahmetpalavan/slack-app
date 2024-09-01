f3bc5bc65a062b1d28c15cc3b9223c8207f99788
Ahmet Palavan <ahmetpalavann@gmail.com>
2024-09-01 21:42:01 +0300

    feat: Add new hook useChannelId to retrieve channel ID from Next.js navigation params.

---
+++ hooks/use-channel-id.ts
@@ -0,0 +1,7 @@
+import { useParams } from 'next/navigation';
+import { Id } from '~/convex/_generated/dataModel';
+
+export const useChannelId = (): Id<'channels'> => {
+  const params = useParams();
+  return params.channelId as Id<'channels'>;
+};
