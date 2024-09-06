280dcb3efa3fee3bce372260ae391f173038b67a
Ahmet Palavan <ahmetpalavann@gmail.com>
2024-09-07 01:36:36 +0300

    feat: Added functionality to manage profile member ID in usePanel hook.

--- hooks/use-panel.ts
+++ hooks/use-panel.ts
@@ -1,15 +1,24 @@
+import { useProfileMemberId } from '~/features/members/store/use-profile-member-id';
 import { useParentMessageId } from '~/features/messages/store/use-parent-message-id';
 
 export const usePanel = () => {
   const [parentMessageId, setParentMessageId] = useParentMessageId();
+  const [profileMemberId, setProfileMemberId] = useProfileMemberId();
+
+  const openProfile = (memberId: string) => {
+    setProfileMemberId(memberId);
+    setParentMessageId(null);
+  };
 
   const openParentMessage = (id: string) => {
     setParentMessageId(id);
+    setProfileMemberId(null);
   };
 
   const closeParentMessage = () => {
     setParentMessageId(null);
+    setProfileMemberId(null);
   };
 
-  return { parentMessageId, openParentMessage, closeParentMessage };
+  return { parentMessageId, profileMemberId, openParentMessage, closeParentMessage, openProfile };
 };
